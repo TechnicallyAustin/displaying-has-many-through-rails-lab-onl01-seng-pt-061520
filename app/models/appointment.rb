71a1581352f060a602421887a70b953ea993783b
Technicallyaustin <Technicallyaustin@gmail.com>
2020-11-18 12:10:16 -0800

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -10,4 +10,10 @@ class Appointment < ApplicationRecord
         self.doctor.name
     end
 
+    def clear_date
+        #method to convert Datetime into Month_name, Day, Year and current time
+        self.appointment_datetime.strftime('%B %d, %Y at %H:%M')
+
+    end
+
 end
